32b34bd86409c7adb8e81f84646f38cebee6e2bc
Jonny <jonny.trafford@gmail.com>
2025-11-15 14:18:37 -0500

    updated parser to output steps

--- helium-service/src/main/java/org/johnnuy/helium/service/BidPackageParser.java
+++ helium-service/src/main/java/org/johnnuy/helium/service/BidPackageParser.java
@@ -2,8 +2,13 @@ package org.johnnuy.helium.service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
-import java.io.StringReader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,48 +23,53 @@ public class BidPackageParser implements PairingProvider {
 
 	private static Pattern TRIP_LINE = Pattern.compile("(TRIP\s#([0-9]+))(.*)");
 	private static Pattern PAIRING_LINE = Pattern.compile("[T][0-9A-Z]{4}");
-	
+
+	private String name;
+	private Path tmpDir;
 	private BufferedReader reader;
 	private Integer currentTrip = null;
-	
-	public BidPackageParser(Reader reader) {
-		this.reader = new BufferedReader(reader);
+
+	public BidPackageParser(String name, Path tmpDir, Reader reader) {
+		this.name = name;
+		this.tmpDir = tmpDir;
 		try {
+			Files.createDirectories(tmpDir);
+			this.reader = new BufferedReader(reader);
+			
 			reformat();
 			skipToNextPairing();
-		}
-		catch(IOException ioe) {
+		} catch (IOException ioe) {
 			throw new RuntimeException(ioe);
 		}
 	}
-	
+
 	@Override
 	public Optional<Pairing> nextPairing() {
 		if (currentTrip == null) {
 			return Optional.empty();
 		}
 		try {
-			Pairing pairing = new Pairing()
-					.setTripNumber(currentTrip);
-			
+			Pairing pairing = new Pairing().setTripNumber(currentTrip);
+
 			/* next line contains the pairing id */
 			String pairingId = reader.readLine();
 			if (!PAIRING_LINE.matcher(pairingId).matches()) {
 				throw new RuntimeException("Unable to identify pairingId for trip %d".formatted(currentTrip));
 			}
 			pairing.setPairingId(pairingId);
-			
+
 			skipToNextPairing();
-			
+
 			return Optional.of(pairing);
-		}
-		catch(IOException ioe) {
+		} catch (IOException ioe) {
 			throw new RuntimeException(ioe);
 		}
 	}
-	
+
 	/**
-	 * helper method that searches for the next TRIP_LINE for identifying the start of a new pairing
+	 * helper method that searches for the next TRIP_LINE for identifying the start
+	 * of a new pairing
+	 * 
 	 * @throws IOException
 	 */
 	private void skipToNextPairing() throws IOException {
@@ -73,24 +83,38 @@ public class BidPackageParser implements PairingProvider {
 		}
 		currentTrip = null;
 	}
-	
+
 	/**
-	 * reformats the file to allow it to parse easily
+	 * prepares the file for parsing by reformatting it properly
+	 * 
+	 * @throws IOException
 	 */
 	private void reformat() throws IOException {
-		StringBuilder buffer = new StringBuilder();
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			Matcher m = TRIP_LINE.matcher(line);
-			if (m.matches() && StringUtils.isNotBlank(m.group(3))) {
-				/* here we have a mixed line, split this up */			
-				buffer.append(m.group(1) + "\n");
-				buffer.append(m.group(3) + "\n");
-			}
-			else {
-				buffer.append(line + "\n");
+		reformat_step1();
+	}
+
+	/**
+	 * Step 1 of the reformatting is used to ensure the TRIP # is on it's own line
+	 * 
+	 * @throws IOException
+	 */
+	private void reformat_step1() throws IOException {
+		Path step1 = tmpDir.resolve("%s_step1.txt".formatted(name));
+		Files.createDirectories(step1.getParent());
+		try (OutputStream stream = Files.newOutputStream(step1, StandardOpenOption.CREATE,
+				StandardOpenOption.TRUNCATE_EXISTING); Writer out = new OutputStreamWriter(stream)) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				Matcher m = TRIP_LINE.matcher(line);
+				if (m.matches() && StringUtils.isNotBlank(m.group(3))) {
+					/* here we have a mixed line, split this up */
+					out.write(m.group(1) + "\n");
+					out.write(m.group(3) + "\n");
+				} else {
+					out.write(line + "\n");
+				}
 			}
 		}
-		this.reader = new BufferedReader(new StringReader(buffer.toString()));
+		this.reader = Files.newBufferedReader(step1);
 	}
 }
--- helium-service/src/test/java/org/johnnuy/helium/service/BidPackageParserTest.java
+++ helium-service/src/test/java/org/johnnuy/helium/service/BidPackageParserTest.java
@@ -3,6 +3,7 @@ package org.johnnuy.helium.service;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 import java.util.Optional;
 
 import org.johnnuy.helium.service.domain.Pairing;
@@ -16,7 +17,10 @@ public class BidPackageParserTest {
 		InputStream source = BidPackageParserTest.class.getClassLoader().getResourceAsStream("december_2025.txt");
 		Assertions.assertNotNull(source);
 		
-		BidPackageParser parser = new BidPackageParser(new InputStreamReader(source));
+		BidPackageParser parser = new BidPackageParser(
+				"december_2025",
+				Path.of("./target", "tmp"),
+				new InputStreamReader(source));
 		
 		Optional<Pairing> pairing;
 		
